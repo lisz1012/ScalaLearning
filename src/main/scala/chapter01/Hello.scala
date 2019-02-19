a64537d089df0cf3eb100565479f438136219eb4
lisz1012 <lisz1012@163.com>
2019-02-18 22:07:26 -0800

    Format, escape and println

--- src/main/scala/chapter01/Hello.scala
+++ src/main/scala/chapter01/Hello.scala
@@ -2,6 +2,14 @@ package chapter01
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello scala!")
+    var n : Int = 8
+    var n2 : Int = 10
+    println(n + n2);println(n * n2 + "\n")
+    print((n + n2) + "\t" + (n * n2) + "\n")
+    println("\"k\"")
+    println("C:\\Users\\lisz1012\\")
+    println("你好\nHello!")
+    println("abc\re")
+    println("姓名\t年龄\t籍贯\t住址\nJohn\t10\t河北\t北京")
   }
 }
